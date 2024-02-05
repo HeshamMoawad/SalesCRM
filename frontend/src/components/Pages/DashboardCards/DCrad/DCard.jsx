d9255b24b52b7e52d01cd88c1f38c12c0a5baff0
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-05 08:36:43 +0200

    :zap: cards comp

---
+++ frontend/src/components/Pages/DashboardCards/DCrad/DCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./DCard.css";
+const DCard = () => {
+    return (
+        <div className='d-card'>
+            
+            card
+        </div>
+    );
+}
+
+export default DCard;
