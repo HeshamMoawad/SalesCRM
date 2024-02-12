b5d5c3ccf34c40f56c2c3abb7ee0ee8dc5c89c60
HeshamMoawad <heshammowad120120@yahoo.com>
2024-02-12 12:56:05 +0200

    :zap: set super user to get full access

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -3,11 +3,15 @@ from .models import BaseUser
 
 class IsManager(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == BaseUser.Role.MANAGER
+        if request.user.is_superuser :
+            return True
+        return request.user.role == BaseUser.Role.MANAGER 
     
 class IsAgent(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == BaseUser.Role.CS | request.user.role == BaseUser.Role.SALES
+        if request.user.is_superuser :
+            return True
+        return request.user.role == BaseUser.Role.CS or request.user.role == BaseUser.Role.SALES 
     
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
