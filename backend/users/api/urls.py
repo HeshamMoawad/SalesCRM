174b50577eecb741c5b2c152eaff0fb26be15848
HeshamMoawad <heshammowad120120@yahoo.com>
2024-02-12 12:55:17 +0200

    :zap: connect views with urls

--- backend/users/api/urls.py
+++ backend/users/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path , include
-from .views import (
-    loginin,
+from rest_framework.authtoken.views import ObtainAuthToken
+from ..views import (
+    test,
+    loginView , 
+    logoutView
     )
 
+
 urlpatterns = [
-    path("test",loginin)
+    path("test/",test) ,
+    path('login/', loginView, name='login'),
+    path('login/', logoutView, name='login'),
+
 ]
+
