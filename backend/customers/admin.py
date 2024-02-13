1b467b3309546264d8832fc941411fc835d42fe7
HeshamMoawad <heshammowad120120@yahoo.com>
2024-02-13 13:52:20 +0200

    :zap: add all models to admin

--- backend/customers/admin.py
+++ backend/customers/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer , Subscription , SubscriptionNote , SubscriptionUpdatesRecord ,CustomerUpdatesRecord
 # Register your models here.
 
 
-admin.site.register(Customer)
+class CustomerAdminSite(admin.ModelAdmin):
+    list_display = ("name" , 'phone', "project" ,"creator")
+    list_filter = ("project","creator" )
+
+class SubscriptionAdminSite(admin.ModelAdmin):
+    list_display = ( "customer" , "creator" ,"cs","price","collected_price")
+    list_filter = ("cs","creator")
+
+class SubscriptionNoteAdminSite(admin.ModelAdmin):
+    list_display = (  "subscription" ,"creator","note","created_at")
+    list_filter = ("subscription","creator")
+
+class SubscriptionUpdatesRecordAdminSite(admin.ModelAdmin):
+    list_display = ( "user" , "subscription" ,"created_at")
+    list_filter = ("subscription" ,)
+
+class CustomerUpdatesRecordAdminSite(admin.ModelAdmin):
+    list_display = ( "user" , "customer" ,"created_at")
+    list_filter = ("customer",)
+
+
+admin.site.register(Customer , CustomerAdminSite)
+admin.site.register(Subscription , SubscriptionAdminSite)
+admin.site.register(SubscriptionNote , SubscriptionNoteAdminSite)
+admin.site.register(SubscriptionUpdatesRecord , SubscriptionUpdatesRecordAdminSite)
+admin.site.register(CustomerUpdatesRecord , CustomerUpdatesRecordAdminSite)
