aabf943c2e7e01c103aee351482dfed7857ca33f
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-20 09:32:11 +0200

    :zap: create customer page

---
+++ frontend/src/Pages/Customers/Customers.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import MainLayout from '../../Layout/MainLayout';
+import "./Customers.css";
+import Pageination from '../../components/Pageination/Pageination';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import CustomerCard from '../../components/CustomerCard/CustomerCard';
+import { usePermission } from '../../Hooks';
+
+
+
+const Customers = () => {   
+    const {permission} = usePermission();
+
+    return (
+        <MainLayout initToggle={{
+            Dashboard: false,
+            Customers: true,
+            Subscriptions:false,
+        }}>
+            <div className="customers">
+                <SearchBar/>
+                <div className="customers-card-container">
+                    <CustomerCard role={permission.role} key={Math.random()}/>
+                    <CustomerCard role={permission.role} key={Math.random()}/>
+                    <CustomerCard role={permission.role} key={Math.random()}/>
+                    <CustomerCard role={permission.role} key={Math.random()}/>
+                    <CustomerCard role={permission.role} key={Math.random()}/>
+                    {/* <CustomerCard key={Math.random()}/>
+                    <CustomerCard key={Math.random()}/> */}
+                </div>
+                <Pageination/>
+            </div>
+        </MainLayout>
+    );
+}
+
+export default Customers;
