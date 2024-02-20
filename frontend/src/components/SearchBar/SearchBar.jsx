ead67ffdf666cb199c3d9f589a083b6f6ac37085
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-20 15:47:26 +0200

    :zap: added add button in search bar

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./SearchBar.css";
-
-const SearchBar = () => {
+import AddLogo from "../../assets/icons/add.png";
+const SearchBar = ({addPath}) => {
+    
     return (
         <>
             <div className="search-bar">
-                <input
-                    className="search-input"
-                    type="text"
-                    placeholder='  phone or name or date "dd/mm/yyyy"'
-                />
+                <div className="search">
+                    <input
+                        className="search-input"
+                        type="text"
+                        placeholder='  phone or name or date "dd/mm/yyyy"'
+                    />
+                    <button className="search-btn">search</button>
 
-                <button className="search-btn">search</button>
+                </div>
+
+                <a href={addPath} className="search-add">
+                    <button className="add-btn">
+                        <img src={AddLogo} alt="..."/>
+                    </button>
+                </a>
             </div>
         </>
     );
