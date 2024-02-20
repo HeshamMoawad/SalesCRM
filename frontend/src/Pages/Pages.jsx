2c00b7559862a70e1c7b42029081d73514500bd4
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-20 16:01:12 +0200

    :zap: add customers routes

--- frontend/src/Pages/Pages.jsx
+++ frontend/src/Pages/Pages.jsx
@@ -5,6 +5,10 @@ import DashBoard from './DashBoard/DashBoard';
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
 import Customers from "./Customers/Customers";
 import Subscriptions from "./Subscriptions/Subscriptions";
+import AddCustomer from './Customers/AddCustomer/AddCustomer';
+import EditCustomer from './Customers/EditCustomer/EditCustomer';
+
+// import NotFound from './NotFound/NotFound';
 
 const Pages = () => {
     return (
@@ -13,13 +17,19 @@ const Pages = () => {
                 <Route path='login' element ={<Login/>} />
                 <Route path="" element={<PrivateRoute/>}>
                     <Route path='dashboard/' element ={<DashBoard/>} />
-                    <Route path='customers/' element ={<Customers/>} />
-                    <Route path='subscriptions' element = {<Subscriptions/>}/> 
+                    <Route path='customers' element ={<Customers/>}/>
+                    <Route path='customers/add' element={<AddCustomer/>}/>
+                    <Route path='customers/view/:customerId' element={<EditCustomer view={true}/>}/>
+                    <Route path='customers/edit/:customerId' element={<EditCustomer />}/>
 
+                    <Route path='subscriptions/' element = {<Subscriptions/>}/>
+                    {/* <Route path='subscriptions/add/:customerId' element={}/> */}
+                    {/* <Route path='subscriptions/view/:subscriptionId' element={}/> */}
+                    {/* <Route path='subscriptions/edit/:subscriptionId' element={}/> */}
+
+                    {/* <Route element = {<NotFound/>}/> */}
                     {/* <Route index element={<Default/>}/> */}
-                    {/* 
-                    <Route element = {<NotFound/>}/> 
-                    */}                
+                               
                 </Route>
 
             </Routes>
