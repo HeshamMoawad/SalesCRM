60ceb82cadece85bd208a9d888455902204b509a
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-21 16:20:59 +0200

    :zap: add some customization on search bar to be useful in other components

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./SearchBar.css";
 import AddLogo from "../../assets/icons/add.png";
-const SearchBar = ({addPath}) => {
-    
+const SearchBar = ({ addPath, headerName , setSearch}) => {
+    const inputRef = useRef(null);
     return (
         <>
+            <div className="section-name">
+                <h1>{headerName}</h1>
+            </div>
             <div className="search-bar">
                 <div className="search">
                     <input
                         className="search-input"
                         type="text"
                         placeholder='  phone or name or date "dd/mm/yyyy"'
+                        ref={inputRef}
                     />
-                    <button className="search-btn">search</button>
-
+                    <button className="search-btn" onClick={()=>{setSearch(inputRef.current.value)}}>Search</button>
                 </div>
-
-                <a href={addPath} className="search-add">
-                    <button className="add-btn">
-                        <img src={AddLogo} alt="..."/>
-                    </button>
-                </a>
+                {addPath ? (
+                    <a href={addPath} className="search-add">
+                        <button className="add-btn">
+                            <img src={AddLogo} alt="..." />
+                        </button>
+                    </a>
+                ) : null}
             </div>
         </>
     );
