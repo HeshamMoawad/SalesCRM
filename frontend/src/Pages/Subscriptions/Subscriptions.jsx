6ffbae684488a8f736e5966742ce82f8362735ae
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-21 16:23:22 +0200

    :zap: create subscriptions as static page to prepare create a hook for it

--- frontend/src/Pages/Subscriptions/Subscriptions.jsx
+++ frontend/src/Pages/Subscriptions/Subscriptions.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import MainLayout from '../../Layout/MainLayout';
-
+import SearchBar from '../../components/SearchBar/SearchBar';
+import Pageination from '../../components/Pageination/Pageination';
+import "./Subscriptions.css";
+import Card , {SUBSCRIPTION} from '../../components/Card/Card';
+import { usePermission } from '../../Hooks';
+// import Loading from "../../components/Loading/Loading";
 
 const Subscriptions = () => {
+    const {permission} = usePermission();
     return (
         <MainLayout initToggle={{
             Dashboard: false,
             Customers: false,
             Subscriptions:true,
         }}>
-            <h1>subscriptions</h1>
+            <div className="subscriptions">
+                <SearchBar headerName={"Subscriptions"}/>
+                <div className="subscriptions-card-container">
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    {/* <Loading/> */}
+                </div>
+                <Pageination/>
+            </div>
+
         </MainLayout>
     );
 }
