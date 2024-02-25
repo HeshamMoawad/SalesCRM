9f5272a8c733487fe63117f87ea412253d7cdced
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-25 12:19:22 +0200

    :zap: use datanotfound component

--- frontend/src/Pages/Customers/Customers.jsx
+++ frontend/src/Pages/Customers/Customers.jsx
@@ -7,10 +7,12 @@ import Card from "../../components/Card/Card";
 import Loading from "../../components/Loading/Loading";
 import { usePermission } from "../../Hooks";
 import { useCustomersFetcher } from "../../Hooks/customers";
+import DataNotFound from "../../components/DataNotFound/DataNotFound";
 
 const Customers = () => {
+    // console.log("Customers render")
     const { permission } = usePermission();
-    const { loading, customers, customersFetch, setSearch } = useCustomersFetcher();
+    const { loading, customers, setSearch } = useCustomersFetcher();
 
     return (
         <MainLayout
@@ -29,12 +31,19 @@ const Customers = () => {
                 <div className="customers-card-container">
                     {loading ? (
                         <Loading />
-                    ) : (
-                        customers.map((customer)=>{
+                    ) : customers.length > 0 ? (
+                        
+                        customers.map((customer) => {
                             return (
-                                <Card role={permission.role} key={customer.uuid} data={customer} />
-                            )
+                                <Card
+                                    role={permission.role}
+                                    key={customer.uuid}
+                                    data={customer}
+                                />
+                            );
                         })
+                    ) : (
+                        <DataNotFound/>
                     )}
                 </div>
                 <Pageination />
