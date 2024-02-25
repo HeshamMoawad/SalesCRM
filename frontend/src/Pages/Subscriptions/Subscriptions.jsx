da3ed005fd62fcfe98ff5fa385da0d47fd65bdf6
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-25 12:18:54 +0200

    :zap: add subscriptions fetcher to component

--- frontend/src/Pages/Subscriptions/Subscriptions.jsx
+++ frontend/src/Pages/Subscriptions/Subscriptions.jsx
@@ -1,39 +1,51 @@
-import React from 'react';
-import MainLayout from '../../Layout/MainLayout';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import Pageination from '../../components/Pageination/Pageination';
+import React from "react";
+import MainLayout from "../../Layout/MainLayout";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Pageination from "../../components/Pageination/Pageination";
 import "./Subscriptions.css";
-import Card , {SUBSCRIPTION} from '../../components/Card/Card';
-import { usePermission } from '../../Hooks';
-// import Loading from "../../components/Loading/Loading";
+import Card, { SUBSCRIPTION } from "../../components/Card/Card";
+import { usePermission } from "../../Hooks";
+import { useSubscriptionsFetcher } from "../../Hooks/subscriptions";
+import Loading from "../../components/Loading/Loading";
+import DataNotFound from "../../components/DataNotFound/DataNotFound";
 
 const Subscriptions = () => {
-    const {permission} = usePermission();
+    const { permission } = usePermission();
+    const { loading, subscriptions, setSearch } = useSubscriptionsFetcher();
     return (
-        <MainLayout initToggle={{
-            Dashboard: false,
-            Customers: false,
-            Subscriptions:true,
-        }}>
+        <MainLayout
+            initToggle={{
+                Dashboard: false,
+                Customers: false,
+                Subscriptions: true,
+            }}
+        >
             <div className="subscriptions">
-                <SearchBar headerName={"Subscriptions"}/>
+                <SearchBar headerName={"Subscriptions"} setSearch={setSearch} />
                 <div className="subscriptions-card-container">
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
-                    <Card role={permission.role} type={SUBSCRIPTION}/>
+                    {loading ? (
+                        <Loading />
+                    ) : subscriptions.length > 0 ? (
+                        subscriptions.map((subscription) => {
+                            return (
+                                <Card
+                                    type={SUBSCRIPTION}
+                                    role={permission.role}
+                                    key={subscription.uuid}
+                                    data={subscription}
+                                />
+                            );
+                        })
+                    ) : (
+                        <DataNotFound />
+                    )}
+
                     {/* <Loading/> */}
                 </div>
-                <Pageination/>
+                <Pageination />
             </div>
-
         </MainLayout>
     );
-}
+};
 
 export default Subscriptions;
