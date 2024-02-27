2be7eced78e0722ea45229819af9227ed8216b65
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-27 10:18:43 +0200

    :zap: add new props with improve usage logic

--- frontend/src/components/CustomSelection/CustomSelection.jsx
+++ frontend/src/components/CustomSelection/CustomSelection.jsx
@@ -1,21 +1,56 @@
-import React , {useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./CustomSelection.css";
 
 const CustomSelection = (props) => {
-    const { options , defaultIndex , child } = props;
-    const [selectedValue, setSelectedValue] = useState(defaultIndex ? options[defaultIndex].name : "");
-
+    const { options, defaultIndex, child, setSelection } = props;
+    const [selectedValue, setSelectedValue] = useState(
+        defaultIndex
+            ? options[defaultIndex]?.name
+                ? options[defaultIndex].name
+                : options[defaultIndex].username
+            : ""
+    );
     const handleSelectionChange = (e) => {
         setSelectedValue(e.target.value);
     };
-    
+    useEffect(() => {
+        if (setSelection){
+            setSelection(selectedValue);
+            console.log(selectedValue);
+        }
+    }, [selectedValue]);
     return (
         <div className="custom-selection">
             {child}
-            <select value={selectedValue} onChange={(e)=>{handleSelectionChange(e)}}>
+            <select
+                value={selectedValue}
+                onChange={(e) => {
+                    handleSelectionChange(e);
+                }}
+            >
+                <option key={Math.random()} value={''}>
+                </option>
                 {options
                     ? options.map((option) => {
-                          return <option key={Math.random()} value={option.name}>{option.name}</option>;
+                          if (option?.name) {
+                              return (
+                                  <option
+                                      key={Math.random()}
+                                      value={option.name}
+                                  >
+                                      {option.name}
+                                  </option>
+                              );
+                          } else {
+                              return (
+                                  <option
+                                      key={Math.random()}
+                                      value={option.username}
+                                  >
+                                      {option.username}
+                                  </option>
+                              );
+                          }
                       })
                     : null}
             </select>
