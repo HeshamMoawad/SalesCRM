147ebd4ff1a16e81c5570196d59473af47243fef
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-27 10:15:51 +0200

    :zap: add new time utils

--- frontend/src/utils/time.js
+++ frontend/src/utils/time.js
@@ -11,4 +11,18 @@ export const isLoginExpired = (time)=>{
     const currentDate = new Date();
     return futureDate < currentDate;
   }
+
+export const getDate = (_date , time = true)=>{
+    const date = new Date(_date)
+    const arr = date.toLocaleDateString().split("/")
+    return `${arr[1]}/${arr[0]}/${arr[2]}  ${time ?  date.toLocaleTimeString() : '' }`
+}
+
+export const getDaysBetweenDates = (startDate, endDate) =>{
+    const startTimestamp = startDate.getTime();
+    const endTimestamp = endDate.getTime();
+    const difference = Math.abs(endTimestamp - startTimestamp);
+    const daysDifference = Math.ceil(difference / (1000 * 3600 * 24));
+    return daysDifference;
+  }
   
