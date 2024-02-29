8242b53392c30002b50ea0efcab529013e2a87cc
HeshamMoawad <heshammoawad120120@yahoo.com>
2024-02-29 07:40:44 +0200

    :zap: add buttons logic

--- frontend/src/components/Pageination/Pageination.jsx
+++ frontend/src/components/Pageination/Pageination.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Pageination.css';
 
-const Pageination = (props) => {
-
-    const [currentPage , setCurrentPage] = useState(1)
-
-    useEffect(()=>{
-
-    }, [currentPage])
-
+const Pageination = ({currentPage , setCurrentPage , hasMore }) => {
+    const nextIsDisabled = hasMore.next ? false : true ;
+    const prevIsDisabled = hasMore.prev ? false : true ;
     return (
         <div className='pageination-area'>
             <div className='pageination'>
-                <button className='prev'>
+                <button className='prev' onClick={(e)=>{
+                    setCurrentPage(prev=>{return prev - 1})
+                }} disabled={prevIsDisabled}>
                     Prev
                 </button>
                 <button className='current' disabled >
                     {currentPage}
                 </button>
-                <button className='next'>
+                <button className='next' onClick={(e)=>{
+                    if (hasMore.next){
+                        setCurrentPage(prev=>{return prev + 1})
+                    }
+                }} disabled={nextIsDisabled}>
                     Next
                 </button>
-
+                <label className='count'>Count : {hasMore.count}</label>
             </div>
         </div>
     );
