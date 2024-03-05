41e3fe0638dde7427c44734129c6c34700048a2c
HeshamMoawad <heshammowad120120@yahoo.com>
2024-03-05 09:30:58 +0200

    :zap: add updates signals

--- backend/customers/apps.py
+++ backend/customers/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class CustomersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'customers'
+
+    def ready(self) -> None:
+        from .signals import (
+            update_customer_signal ,
+            update_subscription_signal
+            )
+
+        return super().ready()
