547b638fc843ebf91f38742746f43ea1615d86cf
HeshamMoawad <heshammowad120120@yahoo.com>
2024-03-05 09:31:57 +0200

    :zap: add updates logic for set creator auto

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -5,10 +5,11 @@ from users.models import (
 )
 from uuid import uuid4
 from utils.customer import validate_number
+from datetime import datetime , timedelta
 
 
 class Customer(models.Model):
-    uuid = models.CharField(verbose_name="UUID" , default=uuid4 ,max_length=200)
+    uuid = models.CharField(verbose_name="UUID" , default=uuid4 ,max_length=200 , unique=True)
     creator = models.ForeignKey(BaseUser ,verbose_name="Creator", on_delete=models.SET_NULL , null=True , related_name="customer_creator")
     project = models.ForeignKey(Project  ,verbose_name="Project", on_delete= models.SET_NULL , null=True)
     name = models.CharField(max_length=200,verbose_name="Name" )
@@ -18,6 +19,7 @@ class Customer(models.Model):
 
     def save(self, *args, **kwargs) -> None:
         self.phone = validate_number(self.phone)
+        self.saver = kwargs.pop('user', None)
         return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
@@ -36,22 +38,37 @@ class CustomerUpdatesRecord(models.Model):
 
 
 class Subscription(models.Model):
-    uuid = models.CharField(verbose_name="UUID" , default=uuid4 ,max_length=200)
+    uuid = models.CharField(verbose_name="UUID" , default=uuid4 ,max_length=200 , unique=True)
     creator = models.ForeignKey(BaseUser ,verbose_name="Creator", on_delete=models.SET_NULL , null=True , related_name="subscription_creator")
     customer =  models.ForeignKey(Customer ,verbose_name="Customer", on_delete=models.CASCADE , null=True , related_name="subscription_customer")
+    text = models.TextField(verbose_name="Text"  ,max_length=1500 , blank=True , default='')
     cs = models.ForeignKey(BaseUser ,verbose_name="CS", on_delete=models.SET_NULL , null=True , related_name="subscription_cs")
     start_date = models.DateTimeField(verbose_name="Start Subscription Date")
     end_date = models.DateTimeField(verbose_name="End Subscription Date")
     price = models.IntegerField(verbose_name="Subscription Price")
     collected_price = models.IntegerField(verbose_name="Collected Price")
+    deuration = models.CharField(verbose_name="Deuration" ,max_length=200 ,blank=True )
     created_at = models.DateTimeField(verbose_name="Creation Date & Time",auto_now_add=True)
 
-    def is_unpaid_subscription(self):
-        return (self.price - self.collected_price) > 0
+    def __str__(self) -> str:
+        return f'{self.customer}'
+
+    # def is_unpaid_subscription(self):
+    #     return (self.price - self.collected_price) > 0
+
+    # def is_duration_valid(self):
+    #     return self.end_date.date() >= datetime.now().date()
 
     def save(self, *args, **kwargs) -> None:
         if self.collected_price > self.price :
             self.collected_price = self.price
+        self.saver = kwargs.pop('user', None)
+        created = kwargs.pop('created',True)
+        if self.saver :
+            if created :
+                self.start_date = datetime.strptime(self.start_date.replace(" ",''),'%m/%d/%Y')#-timedelta(days=-1)+'-T%H:%M:%S.%fZ'
+            self.end_date = datetime.strptime(self.end_date.replace(" ",''),'%m/%d/%Y')#-timedelta(days=-1)'%Y-%m-%dT%H:%M:%S.%fZ'
+        
         return super().save(*args, **kwargs)
 
 class SubscriptionNote(models.Model):
@@ -59,6 +76,12 @@ class SubscriptionNote(models.Model):
     creator = models.ForeignKey(BaseUser ,verbose_name="Creator", on_delete=models.SET_NULL , null=True , related_name="subscription_note_creator")
     created_at = models.DateTimeField(verbose_name="Creation Date & Time",auto_now_add=True)
     note = models.CharField(verbose_name="Note" ,max_length=600)
+
+    def save(self, *args, **kwargs) -> None:
+        self.saver = kwargs.pop('user', None)
+        if self.saver != None :
+            self.creator = self.saver
+        return super().save(*args, **kwargs)
 
 class SubscriptionUpdatesRecord(models.Model):
     user = models.ForeignKey(BaseUser ,verbose_name="Creator", on_delete=models.SET_NULL ,null=True )
