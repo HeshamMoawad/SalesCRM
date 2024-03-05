ff6fba8218379f9ceb2b1eeba0fecc3a9b5f5459
HeshamMoawad <heshammowad120120@yahoo.com>
2024-03-05 09:14:29 +0200

    :zap: header auth as default & token visibilty

--- backend/utils/users.py
+++ backend/utils/users.py
@@ -1,4 +1,4 @@
-from users.authentication import CookieAuthentication
+from users.authentication import CookieAuthentication , HeaderAuthentication
 from users.models import BaseUser
 import typing
 
@@ -22,8 +22,13 @@ def get_fieldsets(name:str):
 
 
 
-def get_user_from_request(request,auth_class=CookieAuthentication)-> typing.Optional[BaseUser]:
+def get_user_from_request(request,auth_class=HeaderAuthentication,token=False)-> typing.Optional[BaseUser]:
     auth = auth_class().authenticate(request)
     if auth :
+        if token :
+            return auth
         return auth[0]
+
+    if token :
+        return None , None
     return None
