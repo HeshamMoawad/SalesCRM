d432ba56b063c7a5eaadeb8d19325bba129a4d41
HeshamMoawad <heshammowad120120@yahoo.com>
2024-03-05 09:36:38 +0200

    :zap: add custom pagination with update logic

--- backend/customers/views/customer.py
+++ backend/customers/views/customer.py
@@ -1,31 +1,45 @@
 from rest_framework.views import Response , Request , APIView
 from django.http.request import HttpRequest
 from utils import get_user_from_request , BaseUser
-from permissions import UserCrudPermissions
+from permissions import UserCrudCustomerPermissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-from ..models import Customer , Subscription
+from ..models import Customer 
 from ..api.serializers import CustomerSerializer
 from django.core.exceptions import FieldError , ValidationError 
 from django.db.utils import IntegrityError
+from ..api.pagination import CustomPagination
+from users.models import Project
 
 class CustomerAPIView(APIView):
     permission_classes = [
         IsAuthenticated ,
-        UserCrudPermissions
+        UserCrudCustomerPermissions
     ]
+    pagination_class = CustomPagination
 
     # Can See
     def get(self,request:HttpRequest):
         user:BaseUser = get_user_from_request(request)
         filters = request.GET.dict()
+        filters.pop(self.pagination_class.page_query_param,None)
+        filters.pop(self.pagination_class.page_size_query_param,None)
+
         try : 
             if user.is_superuser or user.role == user.Role.MANAGER :
                 customers = Customer.objects.filter(**filters).distinct()
             else :
-                customers = Customer.objects.filter(creator=user ,project=user.project, is_deleted=False , **filters ).distinct() | Customer.objects.filter(is_deleted=False ,project=user.project, subscription_customer__cs=user).distinct()
-            customer_serializer = CustomerSerializer(customers , many=True)
-            return Response(customer_serializer.data)
+                customers = Customer.objects\
+                    .filter(creator=user ,project=user.project, is_deleted=False , **filters )\
+                        .distinct() | Customer.objects\
+                            .filter(is_deleted=False ,project=user.project, subscription_customer__cs=user)\
+                                .distinct() 
+            customers = customers.order_by('-created_at')
+            paginator = self.pagination_class()
+            result_page = paginator.paginate_queryset(customers, request)
+            customer_serializer = CustomerSerializer(result_page , many=True)
+            # return Response(customer_serializer.data)
+            return paginator.get_paginated_response(customer_serializer.data)
         except FieldError as e :
             return Response({"message":"please enter corrent field name"})
         except Exception as e :
@@ -35,16 +49,33 @@ class CustomerAPIView(APIView):
     def post(self,request:HttpRequest):
         user:BaseUser = get_user_from_request(request)
         data:dict = request.data
-        data.update({
+        if user.is_superuser or user.role == user.Role.MANAGER :
+            project_name = data.pop("project",None)
+            if project_name :
+                project = Project.objects.get(name=project_name)
+                user.project = project
+        
+        unchangable = ["uuid","created_at","id","creator"]
+        if not (user.role == user.Role.MANAGER or user.is_superuser) :
+            unchangable.append("is_deleted")
+
+        new_data = {}
+        for key , value in data.items():
+            if key not in unchangable:
+                new_data.update({
+                    key : value
+                })
+        new_data.update({
             "creator":user ,
-            "project" : user.project
+            "project" : user.project 
         })
-        new_customer = Customer(**data)
+
+        new_customer = Customer(**new_data)
         try :
             new_customer.check()
             new_customer.validate_unique()
             self.check_object_permissions(request,new_customer)
-            new_customer.save()
+            new_customer.save(user=user)
             return Response(CustomerSerializer(new_customer).data)
         except ValidationError as e:
             return Response({"message": f"{e.error_dict['__all__'][0]}"})
@@ -73,7 +104,7 @@ class CustomerAPIView(APIView):
                 if key not in unchangable:
                     setattr(customer, key, value)
             self.check_object_permissions(request,customer)
-            customer.save()
+            customer.save(user=user)
             return Response(CustomerSerializer(customer).data)
         except Customer.DoesNotExist :
             return Response({"message":"customer with this uuid not found"})
@@ -90,6 +121,11 @@ class CustomerAPIView(APIView):
     # delete
     def delete(self,request:HttpRequest):
         data:dict = request.data
+        query = request.META.get("QUERY_STRING" , None)
+        if query != None :
+            data.update({
+                "uuid":query.split("=")[-1]
+            })
         if 'uuid' not in data.keys():
             return Response({"message":"uuid must be in request to get customer and delete it"})
         try :
