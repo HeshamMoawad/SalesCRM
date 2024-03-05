bb6d0cf3f48ff06f34100cbc1bed007978512a2d
HeshamMoawad <heshammowad120120@yahoo.com>
2024-03-05 10:00:14 +0200

    :zap: add dashboard view

--- backend/customers/api/urls.py
+++ backend/customers/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path 
 from ..views import (
     CustomerAPIView ,
-    SubscriptionAPIView
+    SubscriptionAPIView , 
+    NotesAPIView ,
+    dashboard
 )
 
 
 urlpatterns = [
-    path("customers/",CustomerAPIView.as_view()) ,
-    path("subscriptions/",SubscriptionAPIView.as_view()) ,
-
+    path("customers",CustomerAPIView.as_view()) ,
+    path("subscriptions",SubscriptionAPIView.as_view()) ,
+    path("notes",NotesAPIView.as_view()) ,
+    path("dashboard",dashboard) ,
 ]
